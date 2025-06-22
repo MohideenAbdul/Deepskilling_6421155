20598ca94fbefb21be1760124ce914405811ed0c
Mohideen Abdul Aziz <mohideenmushathic3@gmail.com>
2025-06-22 13:48:45 +0530

    Add files via upload

---
+++ Module 1 - Design Patterns and Principles/Exercise 9: Implementing the Command Pattern/CommandPatternExample.java
@@ -0,0 +1,70 @@
+public class CommandPatternExample {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(lightOn);
+        remote.pressButton(); // Output: Light is ON
+
+        remote.setCommand(lightOff);
+        remote.pressButton(); // Output: Light is OFF
+    }
+}
+
+interface Command {
+    void execute();
+}
+
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        if (command != null) {
+            command.execute();
+        }
+    }
+}
+
+class Light {
+    public void on() {
+        System.out.println("Light is ON");
+    }
+
+    public void off() {
+        System.out.println("Light is OFF");
+    }
+}
